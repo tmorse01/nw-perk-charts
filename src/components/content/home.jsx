b972cb1a6a602d3ec1464faba847bd55b3c35af1
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2023-12-09 17:20:38 -0800

    home content text

--- src/components/content/home.jsx
+++ src/components/content/home.jsx
@@ -11,20 +11,22 @@ const Home = () => {
         perks on your gear in New World!
       </Typography.Title>
       <Divider />
+      <Typography.Title level={5}>
+        Explore, Analyze, and Master
+      </Typography.Title>
       <Typography.Text>
-        Uncover the Secrets of Perks in New World: A Data-Driven Adventure
-        Awaits Welcome to the Ultimate Resource for Understanding Perks in New
-        World! Explore, Analyze, and Master: Dive deep into the world of New
-        World perks with our comprehensive data visualizations. Understand how
-        each perk shapes your gameplay, and make the most informed decisions to
-        enhance your in-game strategy.
+        Dive deep into the world of New World perks with our comprehensive data
+        visualizations. Understand how each perk shapes your gear, and make the
+        most informed decisions to enhance your in-game strategy.
       </Typography.Text>
       <Divider />
+      <Typography.Title level={5}>
+        Interactive Perk Data Visualiaztions
+      </Typography.Title>
       <Typography.Text>
-        Interactive Perk Explorer: Our dynamic, interactive chart lets you
-        discover the values and impacts of each perk. Filter by categories like
-        combat, crafting, and gathering to see how different perks compare and
-        interact.
+        Our dynamic, interactive chart lets you discover the values and impacts
+        of each perk. Filter by categories like combat, crafting, and gathering
+        to see how each perks effects the value of the item.
       </Typography.Text>
     </div>
   );
